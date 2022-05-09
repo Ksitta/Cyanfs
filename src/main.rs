c0aa6477c8749444f43db79624ddc5a635e3ea69
Nick Cao <nickcao@nichi.co>
2022-05-09 16:57:05 +0800

    Squashed 'sfs/' changes from 783e90c..6e1b0d5
    6e1b0d5 impl fallocate
    d462745 switch to an extent based allocator
    6e1f171 cleanup
    2485f3c fmt
    2411071 add some logging for debug
    c8efe8a impl fsync
    2fc457e add inode cache layer
    7c21208 futher optimize alligned writes
    
    git-subtree-dir: sfs
    git-subtree-split: 6e1b0d512afa96d6739408c4b2fb724bb06dce01

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,13 @@ use fuser::{mount2, MountOption};
 use sfs::SFS;
 
 fn main() {
+    simple_logger::SimpleLogger::new().init().unwrap();
     let options = vec![
         MountOption::FSName("sfs".to_string()),
         MountOption::AllowOther,
         MountOption::AutoUnmount,
         MountOption::DefaultPermissions,
     ];
-    mount2(
-        SFS::new("target/meta", "/dev/nvme0n1p3"),
-        "/tmp/sfs",
-        &options,
-    )
-    .unwrap();
+    let fs: SFS<4096> = SFS::new("target/meta", "/dev/nvme0n1p3", 2048, 2048);
+    mount2(fs, "/tmp/sfs", &options).unwrap();
 }
